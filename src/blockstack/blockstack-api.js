d147a3ce37dc23aea40139112506ff1b3e63db30
antares-va-tech <antares.va.tech@gmail.com>
2020-11-20 23:45:58 -0500

    removed logger

--- src/blockstack/blockstack-api.js
+++ src/blockstack/blockstack-api.js
@@ -62,7 +62,6 @@ export const bsAddSkhubListToSkylinkIdx = async (session, skhubIdList) => {
 };
 
 export const bsAddSkylink = async (session, skylinkObj, person) => {
-console.log("person", person)
     // check if skhubId is present. If new Object, this value will be empty
     if (person == null) {
         return;
